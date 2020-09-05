353766ce5b52bbe96ce096456c750de33f157999
lucas <lucas@Rukasu-PC>
2020-09-05 18:51:16 -0300

    primeiro commit

---
+++ Atividades_Java/src/Atividades_if/Atividade_2.java
@@ -0,0 +1,10 @@
+package Atividades_if;
+
+public class Atividade_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Atividades_Java/src/Atividades_if/Atividade_3.java
@@ -0,0 +1,10 @@
+package Atividades_if;
+
+public class Atividade_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
